dc01c69f33bcba012ba778eb361779e7da8ed4c1
Youness <younessasdad96@gmail.com>
2023-07-20 18:34:19 +0100

    first commit

---
+++ src/main/java/com/youness/trainingsystembackend/TrainingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.youness.trainingsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSystemBackendApplication.class, args);
+    }
+
+}
